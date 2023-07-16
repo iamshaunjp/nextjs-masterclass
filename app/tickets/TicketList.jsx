5fc1d54b68d34930655419c2e355e45daddc234f
Shaun <shaun@thenetninja.co.uk>
2023-07-16 07:48:53 +0100

    lesson-6

---
+++ app/tickets/TicketList.jsx
@@ -0,0 +1,30 @@
+async function getTickets() {
+  const res = await fetch('http://localhost:4000/tickets', {
+    next: {
+      revalidate: 0 // use 0 to opt out of using cache
+    }
+  })
+
+  return res.json()
+}
+
+export default async function TicketList() {
+  const tickets = await getTickets()
+
+  return (
+    <>
+      {tickets.map((ticket) => (
+        <div key={ticket.id} className="card my-5">
+          <h3>{ticket.title}</h3>
+          <p>{ticket.body.slice(0, 200)}...</p>
+          <div className={`pill ${ticket.priority}`}>
+            {ticket.priority} priority
+          </div>
+        </div>
+      ))}
+      {tickets.length === 0 && (
+        <p className="text-center">There are no open tickets, yay!</p>
+      )}
+    </>
+  )
+}
--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,7 +1,10 @@
+import TicketList from "./TicketList";
+
 export default function Tickets() {
   return (
     <main>
       <h2>Tickets</h2>
+      <TicketList />
     </main>
   )
 }
