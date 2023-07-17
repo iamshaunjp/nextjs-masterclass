ca31038cdf6e432b4ff3a17a751c3b08819036a6
Shaun <shaun@thenetninja.co.uk>
2023-07-17 09:36:33 +0100

    lesson-8

--- app/tickets/[id]/page.jsx
+++ app/tickets/[id]/page.jsx
@@ -1,9 +1,27 @@
+import { notFound } from "next/navigation"
+
+export const dynamicParams = true // default val = true
+
+export async function generateStaticParams() {
+  const res = await fetch('http://localhost:4000/tickets')
+
+  const tickets = await res.json()
+ 
+  return tickets.map((ticket) => ({
+    id: ticket.id
+  }))
+}
+
 async function getTicket(id) {
   const res = await fetch(`http://localhost:4000/tickets/${id}`, {
     next: {
       revalidate: 60
     }
   })
+
+  if (!res.ok) {
+    notFound()
+  }
 
   return res.json()
 }
