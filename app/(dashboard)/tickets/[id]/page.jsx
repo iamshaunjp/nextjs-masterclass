41f842b67957b162050028812ad26623d29c3f13
Shaun <shaun@thenetninja.co.uk>
2023-07-18 11:13:49 +0100

    lesson-16

--- app/(dashboard)/tickets/[id]/page.jsx
+++ app/(dashboard)/tickets/[id]/page.jsx
@@ -2,6 +2,17 @@ import { notFound } from "next/navigation"
 
 export const dynamicParams = true // default val = true
 
+export async function generateMetadata({ params }) {
+  const id = params.id
+
+  const res = await fetch(`http://localhost:4000/tickets/${id}`)
+  const ticket = await res.json()
+ 
+  return {
+    title: `Dojo Helpdesk | ${ticket.title}`
+  }
+}
+
 export async function generateStaticParams() {
   const res = await fetch('http://localhost:4000/tickets')
 
--- app/(dashboard)/tickets/page.jsx
+++ app/(dashboard)/tickets/page.jsx
@@ -3,6 +3,10 @@ import TicketList from "./TicketList"
 import Loading from "../loading"
 import Link from "next/link"
 
+export const metadata = {
+  title: 'Dojo Helpdesk | Tickets'
+}
+
 export default function Tickets() {
   return (
     <main>
