409bd5d4cfb7abb806306b221cf4ce1654eb6e92
Shaun <shaun@thenetninja.co.uk>
2023-07-18 05:42:24 +0100

    lesson-13

---
+++ app/(auth)/login/page.jsx
@@ -0,0 +1,7 @@
+export default function Login() {
+  return (
+    <main>
+      <h2 className="text-center">Login</h2>
+    </main>
+  )
+}
---
+++ app/(auth)/signup/page.jsx
@@ -0,0 +1,7 @@
+export default function Signup() {
+  return (
+    <main>
+      <h2 className="text-center">Sign up</h2>
+    </main>
+  )
+}
