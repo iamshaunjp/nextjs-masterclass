3fa7b98bce8051daef765246d9ed561f7d5215eb
Shaun <shaun@thenetninja.co.uk>
2023-07-18 17:04:22 +0100

    lesson-18

--- app/api/tickets/route.js
+++ app/api/tickets/route.js
@@ -1,1 +1,11 @@
-// endpoint = /api/tickets
+import { NextResponse } from "next/server"
+
+export async function GET() {
+  const res = await fetch('http://localhost:4000/tickets')
+
+  const tickets = await res.json()
+
+  return NextResponse.json(tickets, {
+    status: 200
+  })
+}
