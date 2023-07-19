3532eb1008fc37137a60b77a14d244f17c2f2147
Shaun <shaun@thenetninja.co.uk>
2023-07-19 11:58:01 +0100

    lesson-20

--- app/(dashboard)/tickets/create/CreateForm.jsx
+++ app/(dashboard)/tickets/create/CreateForm.jsx
@@ -17,7 +17,7 @@ export default function CreateForm() {
 
     const newTicket = { title, body, priority, user_email: 'mario@netninja.dev' }
 
-    const res = await fetch('http://localhost:3000/api/tickets', {
+    const res = await fetch('http://localhost:4000/tickets', {
       method: "POST",
       headers: {"Content-Type": "application/json"},
       body: JSON.stringify(newTicket)
