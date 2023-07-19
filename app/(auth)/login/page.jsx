0469fba98230d6e6a9d197523d95d54d60119ca2
Shaun <shaun@thenetninja.co.uk>
2023-07-19 17:10:26 +0100

    lesson-23

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,7 +1,20 @@
+"use client"
+
+import AuthForm from "../AuthForm"
+
 export default function Login() {
+
+  const handleSubmit = async (e, email, password) => {
+    e.preventDefault()
+
+    console.log(email, password)
+  }
+[]
   return (
     <main>
       <h2 className="text-center">Login</h2>
+
+      <AuthForm handleSubmit={handleSubmit} />
     </main>
   )
 }
--- app/(auth)/signup/page.jsx
+++ app/(auth)/signup/page.jsx
@@ -1,7 +1,20 @@
+"use client"
+
+import AuthForm from "../AuthForm"
+
 export default function Signup() {
+
+  const handleSubmit = async (e, email, password) => {
+    e.preventDefault()
+
+    console.log(email, password)
+  }
+
   return (
     <main>
       <h2 className="text-center">Sign up</h2>
+
+      <AuthForm handleSubmit={handleSubmit} />
     </main>
   )
 }
