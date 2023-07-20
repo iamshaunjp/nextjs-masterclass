9aae8df32db957ea0048aee1d725c8b2795718c1
Shaun <shaun@thenetninja.co.uk>
2023-07-20 10:29:16 +0100

    lesson-25

--- app/(dashboard)/layout.jsx
+++ app/(dashboard)/layout.jsx
@@ -1,16 +1,10 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
-
 // components
 import Navbar from '@/app/components/Navbar'
 
 export default async function DashboardLayout({ children }) {
-  const supabase = createServerComponentClient({ cookies })
-  const { data } = await supabase.auth.getSession()
-
   return (
     <>
-      <Navbar user={data.session.user} />
+      <Navbar />
       {children}
     </>
   )
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 import Logo from './dojo-logo.png'
 
-export default function Navbar({ user }) {
+export default function Navbar() {
   return (
     <nav>
       <Image
@@ -14,8 +14,7 @@ export default function Navbar({ user }) {
       />
       <h1>Dojo Helpdesk</h1>
       <Link href="/">Dashboard</Link>
-      <Link href="/tickets" className="mr-auto">Tickets</Link>
-      {user && <span>Hello, {user.email}</span>}
+      <Link href="/tickets">Tickets</Link>
     </nav>
   )
 }
