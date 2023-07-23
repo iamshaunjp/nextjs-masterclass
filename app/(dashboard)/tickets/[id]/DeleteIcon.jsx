caaab03f46645a3db5869a8fffc0274fb35ca895
Shaun <shaun@thenetninja.co.uk>
2023-07-23 13:49:52 +0100

    lesson-43

--- app/(dashboard)/tickets/[id]/DeleteIcon.jsx
+++ app/(dashboard)/tickets/[id]/DeleteIcon.jsx
@@ -1,45 +1,26 @@
 "use client"
-import { useState } from 'react'
-import { useRouter } from 'next/navigation'
+import { useTransition } from 'react'
+import { deleteTicket } from '../actions'
 
 // icons & UI
 import { TiDelete } from 'react-icons/ti'
 
 export default function DeleteIcon({ id }) {
-  const [isLoading, setIsLoading] = useState(false)
-  const router = useRouter()
-
-  const handleClick = async () => {
-    setIsLoading(true)
-    
-    const res = await fetch(`http://localhost:3000/api/tickets/${id}`, {
-      method: 'DELETE'
-    })
-    const json = await res.json()
-
-    if (json.error) {
-      console.log(error)
-      setIsLoading(false)
-    }
-    if (!json.error) {
-      router.refresh()
-      router.push('/tickets')
-    }
-  }
+  let [isPending, startTransition] = useTransition()
 
   return (
     <button 
       className="btn-primary" 
-      onClick={handleClick}
-      disabled={isLoading}
+      onClick={() => startTransition(() => deleteTicket(id))}
+      disabled={isPending}
     >
-      {isLoading && (
+      {isPending && (
         <>
           <TiDelete />
           Deleting....
         </>
       )}
-      {!isLoading && (
+      {!isPending && (
         <>
           <TiDelete />
           Delete Ticket
--- app/(dashboard)/tickets/actions.js
+++ app/(dashboard)/tickets/actions.js
@@ -27,3 +27,18 @@ export async function addTicket(formData) {
   revalidatePath('/tickets')
   redirect('/tickets')
 }
+
+export async function deleteTicket(id) {
+  const supabase = createServerActionClient({ cookies })
+
+  const { error } = await supabase.from('tickets')
+    .delete()
+    .eq('id', id)
+  
+  if (error) {
+    throw new Error('Could not delete the ticket.')
+  }
+
+  revalidatePath('/tickets')
+  redirect('/tickets')
+} 
